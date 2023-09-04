665c96b5dfdec468de5e30adc0c81f32e489370f
deivid <101598816+deividbautista@users.noreply.github.com>
2023-09-04 15:41:17 -0500

    Add files via upload

---
+++ Metodo 1/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from datetime import date, timedelta
+
+app = Flask(__name__)
+
+# Lista arbitraria de procesos con fechas límite
+procesos = [
+    {"nombre": "Proceso 1", "fecha_limite": date(2023, 9, 10)},
+    {"nombre": "Proceso 2", "fecha_limite": date(2023, 9, 15)},
+    {"nombre": "Proceso 3", "fecha_limite": date(2023, 9, 5)},
+    {"nombre": "Proceso 4", "fecha_limite": date(2023, 9, 25)},
+    {"nombre": "Proceso 4", "fecha_limite": date(2023, 9, 3)},
+]
+
+# Ruta para mostrar la lista de procesos
+@app.route('/')
+def lista_de_procesos():
+    # Obtener la fecha actual
+    hoy = date.today()
+
+    # Calcular la fecha próxima (por ejemplo, 7 días después de hoy)
+    proximo = hoy + timedelta(days=4)
+
+    return render_template('index.html', procesos=procesos, hoy=hoy, proximo=proximo)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=3030)
+
